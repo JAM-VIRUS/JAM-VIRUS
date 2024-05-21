a06eb31fcb0d37e1f2a9f2b956347e9a31c12cab
JAM-VIRUS <jamvirus.spam@gmail.com>
2024-05-21 22:41:00 +0600

    Update jam.approval .py

--- jam.approval .py
+++ jam.approval .py
@@ -1,11 +1,8 @@
 Jamil 
 14h04h24h34h54hu4h04h_4ha4h24h34h5 
 
-paid - 28 may -Ananta 
+paid -28may- Ananta -50 tk
 14h04h34h54h34hu4h04h_4ha4h34h54h3
-free- 1 day -
-14h04h14h74h24hu4h04h_4ha4h14h74h2
-14h04h14h74h24hu4h04h_4ha4h14h74h2
-trail-paid
-14h04h44h34h84hu4h04h_4ha4h44h34h8
-14h04h14h54h14hu4h04h_4ha4h14h54h1
+
+paid -05june- Liton - 200 tk
+14h04h54h94h84hu4h04h_4ha4h54h94h8
