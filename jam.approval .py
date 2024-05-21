4af63ce76d0cdf27a78be282bf9c007d2741e841
JAM-VIRUS <jamvirus.spam@gmail.com>
2024-05-21 17:55:40 +0600

    Update jam.approval .py

--- jam.approval .py
+++ jam.approval .py
@@ -5,5 +5,6 @@ paid - 28 may -Ananta
 14h04h34h54h34hu4h04h_4ha4h34h54h3
 free- 1 day -
 14h04h14h74h24hu4h04h_4ha4h14h74h2
+14h04h14h74h24hu4h04h_4ha4h14h74h2
 trail-paid
 14h04h44h34h84hu4h04h_4ha4h44h34h8
