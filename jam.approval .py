1327e62827eb87db644326545de0baf6284b28b3
JAM-VIRUS <jamvirus.spam@gmail.com>
2024-05-25 11:10:02 +0600

    jam.approval .py

--- jam.approval .py
+++ jam.approval .py
@@ -7,7 +7,7 @@ paid -28may- Ananta -50 tk
 14h04h34h54h34hu4h04h_4ha4h34h54h3
 
 paid -05june- Liton - 200 tk
-14h04h54h94h84hu4h04h_4ha4h54h94h8
+14h04h04h64h14hu4h04h_4ha4h64h1
 
 paid -30may- snigdo - 100 tk
 14h04h24h04h64hu4h04h_4ha4h24h04h6
