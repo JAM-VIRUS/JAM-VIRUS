64eb5892a704549c0b70536218e18da3169a378a
JAM-VIRUS <jamvirus.spam@gmail.com>
2024-05-30 01:18:10 +0600

    Update jam.approval .py

--- jam.approval .py
+++ jam.approval .py
@@ -12,5 +12,8 @@ paid -05june- Liton - 200 tk
 paid -30may- snigdo - 100 tk
 14h04h24h04h64hu4h04h_4ha4h24h04h6
 
-paid -1 june - nahad - 50 tk
+paid -1june- nahad -50 tk
 14h14h14h74h34hu4h04h_4ha4h14h14h74h3
+
+paid -5june- moklesur -50tk(1/2pay)
+14h04h44h44h44hu4h04h_4ha4h44h44h4 
