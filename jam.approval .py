95ab67fb792bdc7128b9cdd3a5e65505c540d275
JAM-VIRUS <jamvirus.spam@gmail.com>
2024-06-07 16:00:09 +0600

    Update jam.approval .py

--- jam.approval .py
+++ jam.approval .py
@@ -7,7 +7,7 @@ paid -28may- Ananta -50 tk
 EXPIRED 
 
 paid -05june- Liton - 200 tk
-14h04h04h64h14hu4h04h_4ha4h64h1
+Expired 
 
 paid -30may- snigdo - 100 tk
 EXPIRED
@@ -16,10 +16,10 @@ paid -1june- nahad -50 tk
 EXPIRED
 
 paid -5june- moklesur -50tk(1/2pay)
-14h04h44h44h44hu4h04h_4ha4h44h44h4 
+Expired 
 
 Paid -5june- alone lover - 100tk
-14h04h54h84h84hu4h04h_4ha4h54h84h8
+expired
 
 free ashraful
-14h04h34h54h04hu4h04h_4ha4h34h54h0
+Expired 
